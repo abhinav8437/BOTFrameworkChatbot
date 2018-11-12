86995ff3fc04888877a8fb6987dd52edd404e96f
Abhinav Rohilla <abhinavrohilla@Abhinavs-MacBook-Air.local>
2018-11-12 15:27:30 +0530

    slight changes

--- Dialogs/InterestedToBuyCarDialog.cs
+++ Dialogs/InterestedToBuyCarDialog.cs
@@ -1,33 +1,33 @@
-using System;
+﻿using System;
 using System.Configuration;
 using System.Threading.Tasks;
 
+
 using Microsoft.Bot.Builder.Dialogs;
-using Microsoft.Bot.Builder.Luis;
-using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Connector;
 
 namespace Microsoft.Bot.Sample.LuisBot
+{
+    [Serializable]
+    public class InterestedToBuyCarClass : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
 
-
-{	[Serializable]
-	public class InterestedToBuyCarClass : IDialog<object>
-	{
-                public Task StartAsync(IDialogContext context)
-                {
-                        context.Wait(MessageReceivedAsync);
-                   
-                        return Task.CompletedTask;       
-                }
-
-                private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
-                {
-                    var activity = await result as Activity;
-        
-                    await context.PostAsync("you have reached right class");
-        
-                    context.Wait(MessageReceivedAsync);
+            return Task.CompletedTask;
         }
-  
-		
-	}
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            await context.PostAsync("you have reached right class, blah blah blah");
+
+            context.Wait(MessageReceivedAsync);
+        }
+
+
+    }
+}
 }
