cdf4ed03902e2e8a6d2888a1235e109ecc00c459
Abhinav Rohilla <abhinavrohilla@Abhinavs-MacBook-Air.local>
2018-11-12 15:02:18 +0530

    first commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=demobottatras-Web-Deploy.pubxml -p:Password=P8tkqGDCGraATiJrNExee1TlEGWNGBECnvv7DrGTqFXhkJPg8dKRfQqwqrxX
+
